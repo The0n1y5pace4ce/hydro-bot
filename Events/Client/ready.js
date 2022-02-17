4e7eab97f3e9a4846526b1a235a5e85624f40919
The0n1y5pace4ce <spaceacethe625@gmail.com>
2022-02-17 17:23:03 +1100

    first commit

---
+++ Events/Client/ready.js
@@ -0,0 +1,14 @@
+const { Client } = require("discord.js")
+
+
+module.exports = {
+    name: 'ready',
+    once: true,
+    /**
+    * @param {Client} client
+    */
+    execute(client) {
+        console.log("The bot is now ready")
+        client.user.setActivity('Over Hydro OCE\'s Server', {type: 'WATCHING'});
+    }
+}
